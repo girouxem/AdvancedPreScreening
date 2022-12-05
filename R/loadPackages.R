2206054c2c19775ffc286aef22fad18a38e63e48
girouxem <emily.giroux.science@gmail.com>
2022-12-05 15:04:44 -0500

    reorganising loading of required packages and sourcing functions to work with rprojroot

---
+++ R/loadPackages.R
@@ -0,0 +1,22 @@
+r <- getOption("repos")
+r["CRAN"] <- "http://cran.us.r-project.org"
+options(repos = r)
+
+if (!requireNamespace("BiocManager"))
+  install.packages("BiocManager")
+BiocManager::install()
+
+library("BiocManager")
+.cran_packages <- c("cowplot", "data.table", "ggplot2", "knitr", "reticulate", "rprojroot")
+.bioc_packages <- c("ape", "BarcodingR", "BiocStyle", "biomartr", "Biostrings", "dada2",  
+                    "GenomicFeatures","phyloseq", "ShortRead")
+.inst <- .cran_packages %in% installed.packages()
+if(any(!.inst)) {
+  install.packages(.cran_packages[!.inst])
+}
+.inst <- .bioc_packages %in% installed.packages()
+if(any(!.inst)) {
+  BiocManager::install(.bioc_packages[!.inst], ask = FALSE)
+}
+
+sapply(c(.cran_packages, .bioc_packages), require, character.only = TRUE)
