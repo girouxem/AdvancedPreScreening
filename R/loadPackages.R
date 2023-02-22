b65467b5b386354541ce6e20c6234d6dd90025f0
girouxem <emily.giroux.science@gmail.com>
2023-02-22 12:13:38 -0500

    edits to test script and associated files

--- R/loadPackages.R
+++ R/loadPackages.R
@@ -16,8 +16,8 @@ if(any(!.inst)) {
 }
 .inst <- .bioc_packages %in% installed.packages()
 if(any(!.inst)) {
-  # BiocManager::install(.bioc_packages[!.inst], ask = FALSE)
-  BiocManager::install(.bioc_packages[!.inst], ask = FALSE, lib = "/cloud/lib/x86_64-pc-linux-gnu-library/4.2")
+  BiocManager::install(.bioc_packages[!.inst], ask = FALSE)
+  #BiocManager::install(.bioc_packages[!.inst], ask = FALSE, lib = "/cloud/lib/x86_64-pc-linux-gnu-library/4.2")
 }
 
 sapply(c(.cran_packages, .bioc_packages), require, character.only = TRUE)
